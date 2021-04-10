b97a8c696ea9d1dc3e71034241eb657b33965e8a
Joaquin Herrera <joaquinherrera2016@gmail.com>
2021-04-10 15:34:47 -0400

    Commit inicial

---
+++ index.js
@@ -0,0 +1,18 @@
+import { getSismologia, getSismologiaUnica } from './servicios/sismologia.js';
+import express from 'express';
+
+const app = express();
+const port = 3000 || process.env.port;
+
+app.get('/', async (req, res) => {
+    const sismosActuales = await getSismologia("http://sismologia.cl/links/tabla.html");
+    const detallesSismos = [];
+    for(let sismo in sismosActuales['sismos']){
+        let detalles = await getSismologiaUnica("http://sismologia.cl" + sismosActuales['sismos'][sismo].url);
+        detalles.mapa = "http://sismologia.cl" + detalles.mapa;
+        detallesSismos.push(detalles);
+    }
+    res.send(detallesSismos);
+})
+
+app.listen(port, () => console.log(`Api Sismologia escuchando en el puerto ${port}`));
---
+++ servicios/sismologia.js
@@ -0,0 +1,36 @@
+import scrapeIt from 'scrape-it';
+
+export async function getSismologia(url){
+    const scrapingSismologia = await scrapeIt(url, {
+        sismos: {
+            listItem:'tr:not(:first-child)',
+            data:{
+                magnitud:'td:nth-last-child(1)',
+                lugar:'td:nth-last-child(2)',
+                hora: 'td:nth-last-child(3)',
+                url: {
+                    selector: 'td:nth-last-child(3) > a',
+                    attr: "href"
+                }
+            }
+        }
+    }).then(response => response = response.data);
+    return scrapingSismologia;
+}
+
+export async function getSismologiaUnica(url){
+    const scrapingSismologia = await scrapeIt(url, {
+        horaLocal:'tr:nth-last-child(7) > td:nth-last-child(1)',
+        horaUtc:'tr:nth-last-child(6) > td:nth-last-child(1)',
+        latitud: 'tr:nth-last-child(5) > td:nth-last-child(1)',
+        longitud: 'tr:nth-last-child(4) > td:nth-last-child(1)',
+        profundidad: 'tr:nth-last-child(3) > td:nth-last-child(1)',
+        magnitud: 'tr:nth-last-child(2) > td:nth-last-child(1)',
+        referencia: 'tr:nth-last-child(1) > td:nth-last-child(1)',
+        mapa: {
+            selector: '.mapa img',
+            attr: "src"
+        }
+        }).then(response => response = response.data);
+    return scrapingSismologia;
+}
