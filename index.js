c97b68464e3b6fdc35d3a5542df780ddc8f237c7
Joaquin Herrera <joa.herreraa@alumnos.duoc.cl>
2021-04-12 18:02:48 -0400

    Solucion CORS

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+ 
 app.get('/', async (req, res) => {
     const sismosActuales = await getSismologia("http://sismologia.cl/links/tabla.html");
     const detallesSismos = [];
